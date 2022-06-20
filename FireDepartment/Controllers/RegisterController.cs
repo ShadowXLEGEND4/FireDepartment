eae926efcbf53019b19cecaf8c8ac81c4427cbc3
ShadowXLEGEND4 <kuruevh8g@gmail.com>
2022-06-20 15:50:42 +0300

    added register controller

---
+++ FireDepartment/Controllers/RegisterController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FireDepartment.Models;
+
+namespace FireDepartment.Controllers
+{
+    internal class RegisterController
+    {
+        public void AddUser(Fireman f)
+        {
+            using (FireDepartmentEntities fr = new FireDepartmentEntities())
+            {
+                var lastFireman = fr.Firemans.ToList().LastOrDefault();
+                if (lastFireman == null)
+                {
+                    lastFireman = new Fireman();
+                    lastFireman.Id = 0;
+                }
+                f.Id = lastFireman.Id + 1;
+                fr.Firemans.Add(f);
+                fr.SaveChanges();
+            }
+        }
+    }
+}
