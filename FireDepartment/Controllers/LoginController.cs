22d5fa8a913daef275ee101533e7ed1f2558abb0
ShadowXLEGEND4 <kuruevh8g@gmail.com>
2022-06-20 15:37:10 +0300

    added login controller

---
+++ FireDepartment/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FireDepartment.Models;
+
+namespace FireDepartment.Controllers
+{
+    internal class LoginController
+    {
+        public string ShowMessage(string username, string password)
+        {
+            using (FireDepartmentEntities fr = new FireDepartmentEntities())
+            {
+                var existingFireman = fr.Firemans.Where(s => s.Name == username).FirstOrDefault();
+                if (existingFireman != null)
+                {
+                    if (existingFireman.Password == password)
+                    {
+                        return null;
+                    }
+                    else
+                    {
+                        return "Wrong Password!";
+                    }
+                }
+                else
+                {
+                    return "No such User!";
+                }
+            }
+        }
+    }
+}
